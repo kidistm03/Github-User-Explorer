bb961373b48ac8c2afeda3f4f61cf5244106f687
kidistm03 <kidistmeseret1@gmail.com>
2026-09-22 21:38:34 +0300

    add React Router and search page

--- src/App.tsx
+++ src/App.tsx
@@ -1,125 +1,15 @@
-import { useState } from 'react'
-import useGitHubUser from './hooks/useGitHubUser'
-import useGitHubRepos from './hooks/useGitHubRepos'
-import SearchBar from './components/SearchBar'
-import UserCard from './components/UserCard'
-import RepoList from './components/RepoList'
-import SortSelect from './components/SortSelect'
-import LanguageFilter from './components/LanguageFilter'
-import useSearchHistory from './hooks/useSearchHistory'
-import SearchHistory from './components/SearchHistory'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import SearchPage from './pages/SearchPage'
 import NotFound from './pages/NotFound'
 
 function App() {
-  const [sortBy, setSortBy] = useState<string>('stars')
-
-  const [selectedLanguage, setSelectedLanguage] =
-    useState<string>('all')
-  const {
-    history,
-    addSearch,
-    clearHistory,
-  } = useSearchHistory()
-
-  const {
-    user,
-    loading: userLoading,
-    error: userError,
-    searchUser,
-  } = useGitHubUser()
-
-  const {
-    repos,
-    loading: reposLoading,
-    error: reposError,
-    searchRepos,
-  } = useGitHubRepos()
-
-  const handleSearch = (username: string): void => {
-    addSearch(username)
-    searchUser(username)
-    searchRepos(username)
-  }
-
-  const sortedRepos = [...repos].sort((a, b) => {
-    if (sortBy === 'stars') {
-      return b.stargazers_count - a.stargazers_count
-    }
-
-    if (sortBy === 'forks') {
-      return b.forks_count - a.forks_count
-    }
-
-    if (sortBy === 'updated') {
-      return (
-        new Date(b.updated_at).getTime() -
-        new Date(a.updated_at).getTime()
-      )
-    }
-
-    if (sortBy === 'name') {
-      return a.name.localeCompare(b.name)
-    }
-
-    return 0
-  })
-
-  const languages = Array.from(
-    new Set(
-      repos
-        .map((repo) => repo.language)
-        .filter(
-          (language): language is string =>
-            language !== null
-        )
-    )
-  ).sort()
-
-  const filteredRepos =
-    selectedLanguage === 'all'
-      ? sortedRepos
-      : sortedRepos.filter(
-        (repo) => repo.language === selectedLanguage
-      )
-
   return (
-    <div>
-      <h1>GitHub User Explorer</h1>
-
-      <SearchBar onSearch={handleSearch} />
-      <SearchHistory
-        history={history}
-        onSelect={handleSearch}
-        onClear={clearHistory}
-      />
-
-      {userLoading && <p>Loading user...</p>}
-
-      {userError && <NotFound />}
-
-      {user && <UserCard user={user} />}
-
-      {reposLoading && <p>Loading repositories...</p>}
-
-      {reposError && <p>{reposError}</p>}
-
-      {repos.length > 0 && (
-        <>
-          <SortSelect
-            sortBy={sortBy}
-            onSortChange={setSortBy}
-          />
-
-          <LanguageFilter
-            languages={languages}
-            selectedLanguage={selectedLanguage}
-            onLanguageChange={setSelectedLanguage}
-          />
-
-          <RepoList repos={filteredRepos} />
-        </>
-      )}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SearchPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
