13e326d4bb3a22d6f55e76bde152e273af80e328
kidistm03 <kidistmeseret1@gmail.com>
2026-09-23 23:52:20 +0300

    improve user not found state

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,9 +1,20 @@
 function NotFound() {
   return (
-    <div>
+    <div className="not-found">
+      <div className="not-found-icon">
+        
+      </div>
+
       <h2>User Not Found</h2>
+
       <p>
-        We could not find a GitHub user with that username.
+        We could not find a GitHub user
+        with that username.
+      </p>
+
+      <p>
+        Please check the username and try
+        again.
       </p>
     </div>
   )
